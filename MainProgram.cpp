0436e83babe7931a1faf7ce14320f6ce8bd032a8
Ahmetgorkemkilic <ahmetgorkem.kilic@bahcesehir.edu.tr>
2026-03-24 13:15:08 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -83,7 +83,7 @@ public:
     BankAccount(const string& owner, double initialBalance) {
         // TODO: Validate and set members
     if (owner.empty()){
-        invalid_argument("Owner name cannot be empty");
+        throw invalid_argument("Owner name cannot be empty");
     }
     if(initialBalance < 0){
         throw invalid_argument("Initial balance cannot be negative.");
